d8b089e81ce7bb12517a26ded25f467bfd6cfed2
bellaliu3001 <lhsblue@hotmail.com>
2020-10-26 22:18:02 +1100

    changed UI component to stateless component

--- src/TodolistUI.js
+++ src/TodolistUI.js
@@ -1,33 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Input, Button, List } from 'antd';
 
-export default class TodolistUI extends Component {
-  render() {
-    return (
-      <div style={{ marginTop: "10px", marginLeft: "10px" }}>
-        <div>
-          <Input 
-            value={this.props.inputValue} 
-            placeholder="todo info" 
-            style={{width: "300px", marginRight: "10px"}}
-            onChange={this.props.handleInputChange}
-          />
-          <Button 
-            type="primary"
-            onClick={this.props.handleBtnClick}
-          >Add Todo Item</Button>
-        </div>
-        <List
-          style={{ width: "300px", marginTop: "10px" }}
-          bordered
-          dataSource={this.props.list}
-          renderItem={(item, index) => (
-            <List.Item onClick={(e) => {this.props.handleItemDelete(e, index)}}>
-              {item}
-            </List.Item>
-          )}
+// stateless component
+const TodolistUI = (props) => {
+  return (
+    <div style={{ marginTop: "10px", marginLeft: "10px" }}>
+      <div>
+        <Input 
+          value={props.inputValue} 
+          placeholder="todo info" 
+          style={{width: "300px", marginRight: "10px"}}
+          onChange={props.handleInputChange}
         />
+        <Button 
+          type="primary"
+          onClick={props.handleBtnClick}
+        >Add Todo Item</Button>
       </div>
-    )
-  }
+      <List
+        style={{ width: "300px", marginTop: "10px" }}
+        bordered
+        dataSource={props.list}
+        renderItem={(item, index) => (
+          <List.Item onClick={(e) => {props.handleItemDelete(e, index)}}>
+            {item}
+          </List.Item>
+        )}
+      />
+    </div>
+  )
 }
+
+export default TodolistUI;
